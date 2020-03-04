f389f589edf6f0522dd51a699164d6304932a072
trio0819 <trio0819@gmail.com>
2020-03-04 15:34:44 +0900

    爆弾を作成。とりあえず、スペースキーで発射し、木にぶつかれば消滅。

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,21 @@
+﻿// 製作者；塚野莉央
+// 爆弾
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Translate(0, 0.2f, 0);
+    }
+
+    //木にぶつかったら消滅
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿// 製作者：塚野莉央
+// プレイヤーが爆弾を発射する処理
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject BombPrefab;
+
+    // とりあえず、スペースキーを押したら爆弾発射
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(BombPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
