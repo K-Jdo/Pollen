3f97b6f626f08daf2759eaf22b6db52f5491eed6
trio0819 <trio0819@gmail.com>
2020-03-25 15:54:33 +0900

    拠点の上に乗ればログに反映されるようにしました。

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,36 @@ public class Player : MonoBehaviour
 {
     public GameObject BombPrefab;
 
-    // とりあえず、スペースキーを押したら爆弾発射
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // 攻撃
+        if (Input.GetKeyDown("joystick button 1"))
         {
             Instantiate(BombPrefab, transform.position, Quaternion.identity);
         }
+
+        // 回復
+        if(Input.GetKeyDown("joystick button 4") && Input.GetKeyDown("joystick button 0"))
+        {
+            Debug.Log("Cure");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 拠点に入ったとき
+        Debug.Log("Enter");
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // 拠点にいるとき
+        Debug.Log("Stay");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // 拠点から出たとき
+        Debug.Log("Exit");
     }
 }
