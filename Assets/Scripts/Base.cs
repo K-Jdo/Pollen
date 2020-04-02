70ccde2b39ba7eac57b842cdccda8acb97262788
trio0819 <trio0819@gmail.com>
2020-04-02 16:05:34 +0900

    拠点の仮処理と爆弾のスプライトを置き換え

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,26 @@
+﻿// 製作者：塚野
+// 拠点
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    public Sprite sprite;
+
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        // if文の中に画像を変える条件を書く
+        if(Input.GetKeyDown("joystick button 2"))
+        {
+            spriteRenderer.sprite = sprite;
+        }        
+    }
+}
