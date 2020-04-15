ea6f6bb488fc6a225cb347fe4082264289a1e303
Meat_Ano <jo-kazu@outlook.jp>
2020-04-15 14:10:57 +0900

    音の追加、サウンドのスクリプトの軽い修正

--- Assets/Scripts/Utility/SoundManager.cs
+++ Assets/Scripts/Utility/SoundManager.cs
@@ -11,17 +11,17 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
 
     public enum SoundName
     {
-        notUsed,    // 拠点使用不可
-        click,      // ボタン選択
-        bomb,       // 爆発の効果音
-        charge,     // 武器補充
-        damage,     // 花粉に当たった音
-        attack,     // 網を振る音
-        shoot,      // 武器を投げる
-        clear,      // クリア判定
-        defeat,     // 敗北判定
-        end,        // ゲーム終了
-        start       // ゲーム開始
+        bomb,           // 爆弾の効果音
+        charge,         // 武器補充
+        clear,          // クリア判定
+        click,          // ボタン選択
+        damage,         // 花粉に当たった音
+        defeat,         // 敗北判定
+        end,            // ゲーム終了
+        area_not_used,  // 拠点使用不可
+        get_pollen,     // 網を振った音
+        shot,           // 爆弾を投げる音
+        start           // ゲーム開始
     }
 
     protected override void Awake()
